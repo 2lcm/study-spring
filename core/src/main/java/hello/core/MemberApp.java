512be24d7aafc5666f267b586507a125c275549c
2lcm <osn011200@naver.com>
2022-02-26 16:19:15 +0900

    2022/02/26

--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -19,6 +19,5 @@ public class MemberApp {
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("findMember = " + findMember.getName());
-
     }
 }
--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -6,15 +6,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("fixDiscountPolicy")
-public class FixDiscountPolicy implements DiscountPolicy {
+@Qualifier("mainDiscountPolicy")
+public class RateDiscountPolicy implements DiscountPolicy{
 
-    private int discountFixAmount = 1000;
+    private int discountPercent = 10;
 
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP){
-            return discountFixAmount;
+            return price * discountPercent / 100;
         } else {
             return 0;
         }
