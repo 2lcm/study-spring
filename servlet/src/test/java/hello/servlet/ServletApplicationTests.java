377e6980fbc3efd814381388650393e8456e314a
2lcm <osn011200@naver.com>
2022-03-13 20:08:41 +0900

    2022/3/13

--- servlet/src/test/java/hello/servlet/ServletApplicationTests.java
+++ servlet/src/test/java/hello/servlet/ServletApplicationTests.java
@@ -2,6 +2,7 @@ package hello.servlet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootTest
 class ServletApplicationTests {
